2560221a108c7548eb9af13f70d2a8ba22ed0425
harjoth <harjoth.khara@gmail.com>
2020-10-13 16:48:46 -0700

    changing attr function to an arrow function

--- index.js
+++ index.js
@@ -5,17 +5,6 @@ const svg = d3.select('svg');
 const rect = svg
   .select('rect')
   .data(data)
-  .attr('width', function (d, i, n) {
-    console.log(i); // 0 - index of current rect element inside of the array
-    console.log(n); // current selection, i.e currently the 1 rect
-    return d.width;
-  })
-  .attr('height', function (d) {
-    console.log(d);
-    return d.height;
-  })
-  .attr('fill', function (d) {
-    return d.fill;
-  });
-
-console.log(rect);
+  .attr('width', (d, i, n) => d.width)
+  .attr('height', (d) => d.height)
+  .attr('fill', (d) => d.fill);
