09563a5b0cec6b135ec86e365ec85a695b367936
harjoth <harjoth.khara@gmail.com>
2020-10-12 18:03:21 -0700

    selecting elements

---
+++ index.js
@@ -0,0 +1,9 @@
+// selecting items from the DOM
+// const a = document.querySelectorAll('div');
+// // using d3
+// const b = d3.selectAll('div');
+
+// console.log(a, b);
+
+const canvas = d3.select('.canvas');
+console.log(canvas);
