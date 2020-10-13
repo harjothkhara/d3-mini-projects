55a727d8f6a543df19023a503166800e5c34adae
harjoth <harjoth.khara@gmail.com>
2020-10-13 13:42:13 -0700

    adding attributes using method chaining

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const canvas = d3.select('.canvas');
 
-const svg = canvas.append('svg');
+// append svg container (return value) with width and height attribute
+const svg = canvas.append('svg').attr('width', 600).attr('height', 600);
 
-// append shapes to svg container
-svg.append('rect');
-svg.append('circle');
-svg.append('line');
+// method chaining to add attributes to svg container
+svg
+  .append('rect')
+  .attr('width', 200)
+  .attr('height', 100)
+  .attr('fill', 'blue')
+  .attr('x', 20)
+  .attr('y', 20);
+
+svg
+  .append('circle')
+  .attr('r', 50)
+  .attr('cx', 300)
+  .attr('cy', 70)
+  .attr('fill', 'pink');
+
+svg
+  .append('line')
+  .attr('x1', 370)
+  .attr('x2', 400)
+  .attr('y1', 20)
+  .attr('y2', 120)
+  .attr('stroke', 'red');
