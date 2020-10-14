62159eddac425b73cdf3bafe86b27cc887c9792c
harjoth <harjoth.khara@gmail.com>
2020-10-14 14:01:40 -0700

    using the enter selection to dynamically add shapes to data from a db

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const data = [
 
 const svg = d3.select('svg');
 
-const rect = svg
-  .selectAll('rect')
-  .data(data)
+// joining the data to rects
+const rects = svg.selectAll('rect').data(data);
+
+// updating the shapes already in the DOM (add attrs to rects already in the DOM)
+rects
   .attr('width', (d, i, n) => d.width)
   .attr('height', (d) => d.height)
   .attr('fill', (d) => d.fill);
 
-console.log(rect);
+// updating the stuff that hasn't entered the DOM through the enter selection and then adding them to the DOM (append the enter selection to DOM)
+rects
+  .enter()
+  .append('rect')
+  .attr('width', (d, i, n) => d.width)
+  .attr('height', (d) => d.height)
+  .attr('fill', (d) => d.fill);
