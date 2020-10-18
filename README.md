dc24147391abb1abca9b425ea04011edeefd3985
harjoth <harjoth.khara@gmail.com>
2020-10-17 18:08:41 -0700

    creating an update function, updated reaadMe with template for updater patern

--- README.md
+++ README.md
@@ -10,4 +10,26 @@
 
 ### Using Data with D3
 
-`
+- Updater Pattern
+
+```javascript
+const update = (data) => {
+  // 1. update scales (domains) if they rely on our data
+  y.domain([0, d3.max(data, (d) => d.orders)]);
+
+  // 2. join updated data to elements
+  const rects = graph.selectAll('rect').data(data);
+
+  // 3. remove unwanted (if any) shapes using the exit selection
+  rects.exit().remove();
+
+  // 4. update the current shapes in the dom
+  rects.attr(...etc);
+
+  // 5. append the enter selection to the dom
+  rects
+    .enter()
+    .append('rect')
+    .attr(...etc);
+};
+```
--- index.js
+++ index.js
@@ -23,6 +23,63 @@ const xAxisGroup = graph
 
 const yAxisGroup = graph.append('g');
 
+// scales
+// linear scale
+const y = d3.scaleLinear().range([graphHeight, 0]);
+
+// band scale
+const x = d3.scaleBand().range([0, 500]).paddingInner(0.2).paddingOuter(0.2);
+
+// create the axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3
+  .axisLeft(y)
+  .ticks(3)
+  .tickFormat((d) => d + ' orders');
+
+// update x axis text
+xAxisGroup
+  .selectAll('text')
+  .attr('transform', 'rotate(-40)')
+  .attr('text-anchor', 'end')
+  .attr('fill', 'orange');
+
+// update function
+const update = (data) => {
+  // updating scale domains
+  y.domain([0, d3.max(data, (d) => d.orders)]);
+  x.domain(data.map((item) => item.name));
+
+  // join the data to rects
+  const rects = graph.selectAll('rect').data(data);
+
+  // remove exit selection
+  rects.exit().remove();
+
+  // update current shapes in dom - adding attrs to all the existing rects in the DOM
+  rects
+    .attr('width', x.bandwidth)
+    .attr('height', (d) => graphHeight - y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', (d) => x(d.name))
+    .attr('y', (d) => y(d.orders));
+
+  // append the enter selection (any DOM elements that need to be added when the joined array is longer than the selection). allows us to get access to virtual elements inside the enter selection
+  rects
+    .enter()
+    .append('rect')
+    .attr('width', x.bandwidth) // scaleband gives us this
+    .attr('height', (d) => graphHeight - y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', (d) => x(d.name))
+    .attr('y', (d) => y(d.orders));
+
+  // call axes
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+};
+
+// get data from firestore
 db.collection('dishes')
   .get()
   .then((res) => {
@@ -30,90 +87,6 @@ db.collection('dishes')
     res.docs.forEach((doc) => {
       data.push(doc.data());
     });
-    console.log(data);
 
-    // linear scale
-    const y = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, (d) => d.orders)])
-      .range([graphHeight, 0]);
-
-    // const min = d3.min(data, (d) => d.orders);
-    // const max = d3.max(data, (d) => d.orders);
-    // const extent = d3.extent(data, (d) => d.orders); // array with min and max
-
-    // console.log(min, max, extent);
-
-    // console.log(y(400));
-    // console.log(y(0));
-    // console.log(y(900));
-
-    // band scale
-    const x = d3
-      .scaleBand()
-      .domain(data.map((item) => item.name))
-      .range([0, 500])
-      .paddingInner(0.2)
-      .paddingOuter(0.2);
-
-    //console.log(x('veg curry'));
-    //console.log(x('veg pasta'));
-    //console.log(x.bandwidth()); // returns the width of every bar
-
-    // join the data to rects
-    const rects = graph.selectAll('rect').data(data);
-    // adding attrs to all the existing rects in the DOM
-    rects
-      .attr('width', x.bandwidth)
-      .attr('height', (d) => graphHeight - y(d.orders))
-      .attr('fill', 'orange')
-      .attr('x', (d) => x(d.name))
-      .attr('y', (d) => y(d.orders));
-
-    // append the enter selection (any DOM elements that need to be added when the joined array is longer than the selection). allows us to get access to virtual elements inside the enter selection
-    rects
-      .enter()
-      .append('rect')
-      .attr('width', x.bandwidth) // scaleband gives us this
-      .attr('height', (d) => graphHeight - y(d.orders))
-      .attr('fill', 'orange')
-      .attr('x', (d) => x(d.name))
-      .attr('y', (d) => y(d.orders));
-
-    // create and call the axes
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3
-      .axisLeft(y)
-      .ticks(3)
-      .tickFormat((d) => d + ' orders');
-
-    xAxisGroup.call(xAxis);
-    yAxisGroup.call(yAxis);
-
-    xAxisGroup
-      .selectAll('text')
-      .attr('transform', 'rotate(-40)')
-      .attr('text-anchor', 'end')
-      .attr('fill', 'orange');
+    update(data);
   });
-
-// d3 updater pattern
-const update = (data) => {
-  // 1. update scales (domains) if they rely on our data
-  y.domain([0, d3.max(data, (d) => d.orders)]);
-
-  // 2. join updated data to elements
-  const rects = graph.selectAll('rect').data(data);
-
-  // 3. remove unwanted (if any) shapes using the exit selection
-  rects.exit().remove();
-
-  // 4. update the current shapes in the dom
-  rects.attr(...etc);
-
-  // 5. append the enter selection to the dom
-  rects
-    .enter()
-    .append('rect')
-    .attr(...etc);
-};
