83e56ed0f6b5d8d79bf8384c5920f15b8169fc14
harjoth <harjoth.khara@gmail.com>
2020-10-17 20:00:14 -0700

    updating our data

--- index.js
+++ index.js
@@ -81,9 +81,29 @@ const update = (data) => {
 
 let data = [];
 // get data from firestore
+// setup real time event listener
 db.collection('dishes').onSnapshot((res) => {
   res.docChanges().forEach((change) => {
-    console.log(change.doc.data());
+    const doc = { ...change.doc.data(), id: change.doc.id };
+    // console.log(doc);
+    // console.log(change);
+
+    switch (change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+
+      case 'modified':
+        const index = data.findIndex((item) => item.id === doc.id);
+        data[index] = doc;
+        break;
+
+      case 'removed':
+        data = data.filter((item) => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
   });
-  //update(data);
+  update(data);
 });
